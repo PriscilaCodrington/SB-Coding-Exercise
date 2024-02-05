54d5f15141ce5b7a2733c622e198e7a028fbaf8a
PriscilaCodrington <priscilacodrington@gmail.com>
2024-02-05 11:16:51 +0100

    function to fetch the info of each id

--- functions.js
+++ functions.js
@@ -1,4 +1,12 @@
-async function getNewsIds(limit = 30) {
+const limit = 150;
+const limit2 = 30;
+
+async function getNewsIds(limit) {
     return fetch(`https://hacker-news.firebaseio.com/v0/newstories.json?limitToFirst=${limit}&orderBy="$key"`)
       .then(response => response.json())
   }
+const getNewById = async (id) =>
+       fetch(`https:hacker-news.firebaseio.com/v0/item/${id}.json`)
+         .then(response => response.json())
+ 
+  
