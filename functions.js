9277b4cd76f3d6b7258c7163b0e69ae58ef2c944
PriscilaCodrington <priscilacodrington@gmail.com>
2024-02-09 20:13:08 +0100

    optimizing load time

--- functions.js
+++ functions.js
@@ -1,87 +1,79 @@
+let news = [];
 
-
-async function getNewsIds(limit= 30) {
-    return fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?limitToFirst=${limit}&orderBy="$key"`)
-      .then(response => response.json())
-  }
-const getNewById = async (id) =>
-       fetch(`https:hacker-news.firebaseio.com/v0/item/${id}.json`)
-         .then(response => response.json())
-
-
-async function getNewsArray(newsIds){
-    const news = [];
-
-    for(const newId of newsIds){
-        const notice = await getNewById(newId);
-        news.push(notice);
-    }
-    
-    return news;
+function getNewsIds(limit = 30) {
+  return fetch(
+    `https://hacker-news.firebaseio.com/v0/topstories.json?limitToFirst=${limit}&orderBy="$key"`
+  ).then((response) => response.json());
 }
-  async function getNews(){
-    const newsIds = await getNewsIds();
-    const allNews = await getNewsArray(newsIds);
-
-    console.log(allNews);
-    renderNewsHtml(allNews);
-
-    filterMoreThan5words(allNews);
-    filterLessThan5words(allNews);
-  }
-  async function renderNewHtml(report) {
-    const container = document.querySelector('.container-grid');
-    const { title, score, descendants, id, by, url } = report
+function getNewById(id) {
+  return fetch(`https:hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+    (response) => response.json()
+  );
+}
+function getNewsArray(newsIds) {
+  const promises = [];
   
-    container.innerHTML += `
+  for (const newId of newsIds) {
+    promises.push(getNewById(newId));
+  }
+
+  return Promise.all(promises);
+}
+async function getNews() {
+  const newsIds = await getNewsIds();
+  news = await getNewsArray(newsIds);
+
+  renderNewsHtml(news);
+}
+function renderNewHtml(report) {
+  const container = document.querySelector(".container-grid");
+  const { title, score, descendants, id, by, url } = report;
+
+  container.innerHTML += `
       <div class="card-body">
          <a href="${url}"> <h4 class="card-title">${title}</h4> </a>
           <p class="card-comments">${descendants} Comments</p>
           <p class="card-points">${score} Points</p>
           <p class="card-order">ID: ${id}</p>
           <p class="card-user">Written by: <b>${by}</b></p>
-      </div>`
-  }
-  async function renderNewsHtml(newsArray) {
-    for (const New of newsArray) {
-      renderNewHtml(New)
-   }
- }
-  
- async function filterMoreThan5words(newsArray) {
-    const filteredNews = newsArray.filter((news) => news.title.split(' ').length > 5);
-    const sortedFilteredNews = filteredNews.sort((a, b) => b.descendants - a.descendants);
-    console.log(sortedFilteredNews);
-
-  return sortedFilteredNews;
+      </div>`;
 }
-document.getElementById('filterMorethan5').addEventListener('click', async () => {
-  const newsIds = await getNewsIds();
-  const allNews = await getNewsArray(newsIds);
-  const filteredNews = await filterMoreThan5words(allNews);
-  renderFilteredNews(filteredNews);
-});
 
+async function renderNewsHtml(newsArray) {
+  const container = document.querySelector(".container-grid");
+  container.innerHTML = "";
+
+  for (const New of newsArray) {
+    renderNewHtml(New);
+  }
+}
+
+async function filterMoreThan5words(newsArray) {
+  const filteredNews = newsArray
+    .filter((news) => news.title.split(" ").length > 5)
+    .sort((a, b) => b.descendants - a.descendants);
+
+  return filteredNews;
+}
 async function filterLessThan5words(newsArray) {
-    const filteredNews = newsArray.filter((news)=> news.title.split(' ').length <= 5);
-    const sortedFilteredNews = filteredNews.sort((a, b) => b.score - a.score);
-    console.log(sortedFilteredNews);
-    return sortedFilteredNews;
-  
-}
-document.getElementById('filterLessthan5').addEventListener('click', async () => {
-  const newsIds = await getNewsIds();
-  const allNews = await getNewsArray(newsIds);
-  const filteredNews = await filterLessThan5words(allNews);
-  renderFilteredNews(filteredNews);
-});
+  const filteredNews = newsArray
+    .filter((news) => news.title.split(" ").length <= 5)
+    .sort((a, b) => b.score - a.score);
 
-async function renderFilteredNews(filteredNews) {
-  const container = document.querySelector('.container-grid');
-  container.innerHTML = ''; // Limpiar contenido anterior
-  for (const news of filteredNews) {
-      await renderNewHtml(news); // Renderizar cada noticia
-  }
+  return filteredNews;
 }
 
-getNews(); 
+document
+  .getElementById("filterMorethan5")
+  .addEventListener("click", async () => {
+    const filteredNews = await filterMoreThan5words(news);
+    renderNewsHtml(filteredNews);
+  });
+document
+  .getElementById("filterLessthan5")
+  .addEventListener("click", async () => {
+    const filteredNews = await filterLessThan5words(news);
+    renderNewsHtml(filteredNews);
+  });
+
+getNews();
